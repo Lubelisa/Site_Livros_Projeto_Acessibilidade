371754b139e711fe2442be75ac5afd63a1c42a6a
Marina Machado <marinamachado@MacBook-de-Marilia.local>
2021-12-04 00:22:14 -0300

    adicionando começo da implementação de contraste e aumento e diminuição de fonte

---
+++ contraste.js
@@ -0,0 +1,17 @@
+function myFunction() {
+var element = document.body;
+element.classList.toggle("dark-mode");
+
+
+}
+
+tamanho = 16;
+function diminuir(){
+  tamanho--;
+  document.body.style.fontSize=tamanho+"px";
+}
+function aumentar(){
+  tamanho++;
+  document.body.style.fontSize=tamanho+"px";
+}
+    
